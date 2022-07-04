f096d8b74a45dc21aaa2995a9cd10c3ca33b7b75
charismaaji <charismakurniawan@gmail.com>
2022-07-04 19:16:40 +0800

    add logout button

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,10 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-export default function Home() {
+export default function Home({navigation}) {
+  const handleLogout = () => {
+    AsyncStorage.clear();
+    navigation.navigate('Login');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home</Text>
+      <TouchableOpacity style={styles.button} onPress={handleLogout}>
+        <Text style={styles.textButton}>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,5 +27,17 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 30,
     color: 'orange',
+  },
+  button: {
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'orange',
+    borderRadius: 5,
+    marginTop: 25,
+  },
+  textButton: {
+    color: 'white',
+    fontWeight: '700',
   },
 });
